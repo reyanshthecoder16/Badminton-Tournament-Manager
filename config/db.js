c020b3265d8f9aa3e74ee3bfa1866a66c4eac35e
reyanshcoder16@gmail.com <reyanshcoder16@gmail.com>
2025-07-21 11:29:40 +0530

    cleanup

--- config/db.js
+++ config/db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || 3306,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.MYSQL_DATABASE || 'badminton',
   logging: false,
 });
 
